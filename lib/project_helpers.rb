1b1be2b02fe714585fa183c611740f05c8eaa59a
Jacques Crocker <railsjedi@gmail.com>
2013-01-20 15:01:06 -0800

    wire up screenshots

--- lib/project_helpers.rb
+++ lib/project_helpers.rb
@@ -11,9 +11,26 @@ module ProjectHelpers
     end
   end
 
-
+  # returns an array of Hashes, one for each associated project screenshot
+  # include fields for :src, :index, :width, and :height
   def project_screenshots(project)
-    # TODO
-    []
+    imgs = []
+
+    files = Dir.glob("#{root}/source/screenshots/#{project.id}/**/screen*", File::FNM_CASEFOLD)
+    files.each_with_index do |file, index|
+      next if File.directory?(file)
+
+      img = {}.with_indifferent_access
+      img[:src] = file.gsub("#{root}/source", "")
+      img[:index] = index+1
+
+      # TODO: set width/height programatically
+      img[:width] = 200
+      img[:height] = 200
+
+      imgs << img
+    end
+
+    imgs
   end
 end
